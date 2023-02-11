abc74cd402f9c49e0ace027199af164c011cad49
rvetoshk <roman.vetoshko@megafon.ru>
2023-02-12 00:46:44 +0400

    Update main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from flask import Flask
-
+from flask_restful import Api
+from src.api.v1.urls import urls
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
+api = Api(app)
 
-@app.route('/hello-world')
-def hello_world():
-    return 'Hello, World!'
+jwt = JWTManager(app)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 
+for resource, url in urls:
+    api.add_resource(resource, url)
 
 if __name__ == '__main__':
     app.run()
