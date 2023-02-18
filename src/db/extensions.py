24f104b2e5a45c827f3b9d3cfda3fcc0463adfea
Seniacat <senya.sidorovich@gmail.com>
2023-02-18 20:44:06 +0300

    related roles table

--- src/db/extensions.py
+++ src/db/extensions.py
@@ -1,12 +1,14 @@
 """Extensions registry"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_restful import Api
 
 
-db = SQLAlchemy()
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
 jwt = JWTManager()
 ma = Marshmallow()
 api = Api()
--- src/db_models.py
+++ src/db_models.py
@@ -1,6 +1,17 @@
 import uuid
+from sqlalchemy import Table, Column, ForeignKey, MetaData
 from sqlalchemy.dialects.postgresql import UUID
+
 from db.postgres import db
+from db.extensions import metadata
+
+
+#related_roles = Table(
+#    'related_roles',
+#    metadata,
+#    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True),
+#    Column("role_id", UUID(as_uuid=True), ForeignKey("roles.id"), primary_key=True)
+#)
 
 
 class User(db.Model):
@@ -9,13 +20,20 @@ class User(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     login = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    roles = db.relationship(
+        "Role",
+        secondary="roles_users",
+        backref=db.backref("users", lazy="dynamic"),
+    )
+    history_auth_id = db.Column(db.Integer, db.ForeignKey('history_auth.id'), nullable=True)
 
     def __repr__(self):
         return f'<User {self.login}>'
 
 
 class Role(db.Model):
-    __tablename__ = 'role'
+    __tablename__ = 'roles'
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
@@ -24,10 +42,9 @@ class Role(db.Model):
         return f'<Role {self.name}>'
 
 
-class RoleUser(db.Model):
-    __tablename__ = 'user_role'
+class RolesUsers(db.Model):
+    __tablename__ = "users_roles"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<Role {self.name}>'
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
+    user_id = db.Column("user_id", UUID(as_uuid=True), db.ForeignKey("users.id"))
+    role_id = db.Column("role_id", UUID(as_uuid=True), db.ForeignKey("roles.id"))
