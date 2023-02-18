67320a849b12b0dcbd7027d60aa83626f4ac3459
Seniacat <senya.sidorovich@gmail.com>
2023-02-18 23:24:20 +0300

    corrected related_roles table

--- src/db_models.py
+++ src/db_models.py
@@ -6,14 +6,6 @@ from db.postgres import db
 from db.extensions import metadata
 
 
-#related_roles = Table(
-#    'related_roles',
-#    metadata,
-#    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True),
-#    Column("role_id", UUID(as_uuid=True), ForeignKey("roles.id"), primary_key=True)
-#)
-
-
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -22,9 +14,10 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String(120), nullable=False)
     roles = db.relationship(
-        "Role",
-        secondary="roles_users",
-        backref=db.backref("users", lazy="dynamic"),
+        'Role',
+        secondary='related_roles',
+        lazy='subquery',
+        backref=db.backref("users", lazy=True),
     )
     history_auth_id = db.Column(db.Integer, db.ForeignKey('history_auth.id'), nullable=True)
 
@@ -42,9 +35,39 @@ class Role(db.Model):
         return f'<Role {self.name}>'
 
 
-class RolesUsers(db.Model):
-    __tablename__ = "users_roles"
+class Device(db.Model):
+    """Модель, описывающая устройство пользователя user."""
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True)
-    user_id = db.Column("user_id", UUID(as_uuid=True), db.ForeignKey("users.id"))
-    role_id = db.Column("role_id", UUID(as_uuid=True), db.ForeignKey("roles.id"))
+    __tablename__ = 'device'
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
+
+    # Через устройство мы можем получить историю авторизаций.
+    history = db.relationship("HistoryAuth", backref='device', lazy=True)
+
+
+class HistoryAuth(db.Model):
+    """Модель, описывающая факт авторизаци пользователем
+    user с устройства device в дату date_auth."""
+
+    __tablename__ = 'history_auth'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+
+    # С каждой авторизацией связан пользователь.
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
+    date_auth = db.Column(db.DateTime, index=True)
+
+    # С каждой авторизацией связано устройство с которого она была выполнена.
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
+
+
+related_roles = Table(
+    'related_roles',
+    metadata,
+    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True),
+    Column("role_id", UUID(as_uuid=True), ForeignKey("roles.id"), primary_key=True)
+)
