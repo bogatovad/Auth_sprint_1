6ef5fb5e030de05976c81ca43761b093f6cfeb4e
Artem Bogatov <artembogatov@MacBook-Pro-Artem.local>
2023-02-20 11:13:26 +0300

    fix

--- src/db/extensions.py
+++ src/db/extensions.py
@@ -1,10 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_jwt_extended import JWTManager
-from flask_marshmallow import Marshmallow
 
 
 metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 jwt = JWTManager()
-ma = Marshmallow()
