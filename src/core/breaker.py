f3ee437dfd7c34992e04f2351644c040052dad68
rvetoshk <roman.vetoshko@megafon.ru>
2023-03-13 00:16:48 +0400

    Add breaker.py

---
+++ src/core/breaker.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+from pybreaker import CircuitBreaker, CircuitBreakerError
+
+
+class CustomCircuitBreakerError(CircuitBreakerError):
+    ERROR_MESSAGE = "We'll be back soon!"
+
+
+breaker = CircuitBreaker(fail_max=3, reset_timeout=30)
+
+
+def handle_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except CustomCircuitBreakerError as e:
+            return {"message": e.ERROR_MESSAGE}, HTTPStatus.SERVICE_UNAVAILABLE
+    return wrapper
